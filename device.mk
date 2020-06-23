931b16f9b7247bb6744958e9b2efe3a142582d7d
merothh <merothh@gmail.com>
2020-06-23 13:18:36 +0530

    picasso: Import keylayouts/idc configs
    * Disable DPAD_HOME
    
    Co-authored-by: Akhil Narang <me@akhilnarang.dev>
    Change-Id: I966feeb406dafe559f4f0c6de7d9916ed7ed96b8

--- device.mk
+++ device.mk
@@ -98,9 +98,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# Keylayout
+# Input
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-goodix.idc
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
 #Light
 PRODUCT_PACKAGES += \
