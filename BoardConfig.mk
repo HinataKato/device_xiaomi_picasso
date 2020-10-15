b555904f0c047318ebf721595b80b9fd4c2f6ac8
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-10-15 20:51:34 +0530

    avicii: Ignore Sepolicy Neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USES_MKE2FS := true
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
