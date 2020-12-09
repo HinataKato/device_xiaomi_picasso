fe0f8f96b1e6a322ff8928c906f1fd5b623b7c0f
xyyx <xyyx@mail.ru>
2020-12-09 09:23:07 +0300

    picasso: Import and overlay WCNSS_qcom_cfg.ini

--- device.mk
+++ device.mk
@@ -262,6 +262,9 @@ PRODUCT_PACKAGES += \
     lineage.trust@1.0-service
 
 # WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
+
 PRODUCT_PACKAGES += \
     WifiOverlay
 
