aa31521dd53fb04fe508fc86a25a59860d73a1e3
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    picasso: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -10,15 +10,13 @@ set -e
 DEVICE=picasso
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2020
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -26,10 +24,9 @@ fi
 source "${HELPER}"
 
 # initialize the helper
-INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
-# Copyright headers and guards
+# Warning headers and guards
 write_headers
 
 # The standard device blobs
