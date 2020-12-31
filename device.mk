cf9988a581e71d13155ac11b3c747e80d592d3c5
Jeong Seong Pil <jjjhitel@gmail.com>
2021-01-01 04:34:21 +0900

    picasso: Build com.dsi.ant@1.0

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
-    com.dsi.ant.antradio_library
+    com.dsi.ant@1.0
 
 # Audio
 PRODUCT_PACKAGES += \
