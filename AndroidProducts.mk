099723206400d134f94c4317c3e8341c318500e0
Jeong Seong Pil <jjjhitel@gmail.com>
2021-01-07 04:33:24 +0900

    picasso: ArrowOS-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_picasso.mk
+    $(LOCAL_DIR)/arrow_picasso.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_picasso-user \
-    lineage_picasso-userdebug 
