df358328e4bc5ea3588fdb63df4e62b02b709242
Jeong Seong Pil <jjjhitel@gmail.com>
2021-01-24 03:19:16 +0900

    picasso: Nuke light HAL

--- device.mk
+++ device.mk
@@ -138,10 +138,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    lights.picasso
-
 # Media
 PRODUCT_PACKAGES += \
     libmediaplayerservice
