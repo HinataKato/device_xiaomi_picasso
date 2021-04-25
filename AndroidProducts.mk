1338ae919e2d62d245afccc522bf1623ed0be6c1
HinataKato <sisijiang233@gmail.com>
2021-04-25 16:58:44 +0000

    picasso: ArrowOS-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,5 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_picasso.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_picasso-user \
-    lineage_picasso-userdebug 
+    arrow_picasso-user \
+    arrow_picasso-userdebug \
+    arrow_picasso-eng
--- arrow_picasso.mk
+++ arrow_picasso.mk
@@ -7,11 +7,11 @@
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_picasso
+PRODUCT_NAME := arrow_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
